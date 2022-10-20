232e3b7c4272a27fba57075178c9d6114f49acee
Micaela Peralta <mika_peralta93@yahoo.com>
2022-10-20 08:09:33 -0300

    Initial commit

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::AuthenticationController < ApplicationController
+   skip_before_action :authentication_request, only: [:login]
+   #POST /auth/login
+   def login
+      @user = User.find_by_email(params[:email])
+      if @user&.authenticate(params[:password])
+         token = jwt_encode(user_id: @user.id)
+         render json: { token: token }, status: :ok
+      else
+         render json: { error: 'Unauthorized' }, status: :unauthorized
+      end
+   end
+end
